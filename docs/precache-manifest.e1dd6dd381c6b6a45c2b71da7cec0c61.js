b74bef90b39112fdbb991b75abbfa3f90c061e29
Ridvan Chasan <ridvan@Ridvans-MBP.fritz.box>
2019-09-29 20:03:08 +0200

    Build in docs folder for deployment

---
+++ docs/precache-manifest.e1dd6dd381c6b6a45c2b71da7cec0c61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db6dc46615623265ba04e755b6cee39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee8f529ba5acb84e0401",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ad1c54a46be91371ebcc",
+    "url": "/static/css/main.f4a3b198.chunk.css"
+  },
+  {
+    "revision": "ee8f529ba5acb84e0401",
+    "url": "/static/js/2.9766327b.chunk.js"
+  },
+  {
+    "revision": "ad1c54a46be91371ebcc",
+    "url": "/static/js/main.e1fea114.chunk.js"
+  },
+  {
+    "revision": "cf687df6f22c1394e253",
+    "url": "/static/js/runtime-main.847da152.js"
+  },
+  {
+    "revision": "e76046a288e593d037950726cec18a4c",
+    "url": "/static/media/bitcoin_icon.e76046a2.png"
+  },
+  {
+    "revision": "3e726c2b6a59e6e4543c0a1534d93796",
+    "url": "/static/media/de.3e726c2b.svg"
+  },
+  {
+    "revision": "d3ddd6025a06a78535b0d432d14905bf",
+    "url": "/static/media/gb.d3ddd602.svg"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/static/media/react_icon.638be909.png"
+  }
+]);
